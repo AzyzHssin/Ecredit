216a55877ecf2bc8dd6e98f6974beb24bed032b3
AzyzHssin <hssine.medaziz1@gmail.com>
2023-11-22 15:43:59 +0100

    pushing front end and backend

---
+++ backend/src/main/java/com/Ecredit/demo/EcreditBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Ecredit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcreditBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcreditBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/Ecredit/demo/EcreditBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecredit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcreditBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
