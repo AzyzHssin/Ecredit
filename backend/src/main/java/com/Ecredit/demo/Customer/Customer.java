a63396179714c60bac74918a83ada7ddccd37e35
AzyzHssin <hssine.medaziz1@gmail.com>
2023-11-27 15:29:39 +0100

    psuhing one model

--- backend/src/main/java/com/Ecredit/demo/Customer/Customer.java
+++ backend/src/main/java/com/Ecredit/demo/Customer/Customer.java
@@ -1,4 +1,20 @@
 package com.Ecredit.demo.Customer;
 
+import com.Ecredit.demo.BankAccount.BankAccount;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Date;
+
+@Entity
+@Table
 public class Customer {
+    @Id
+    private long cin;
+    private String lastName;
+    private String firstName;
+    private Date birthday;
+    private ArrayList<BankAccount> listOfAccounts;
 }
